8a9cd0b7eb6b88676406d506a8e9f1a66adbe8d8
Mahmoud-Magdy17 <MahmoudMagdy176203@gmail.com>
2024-06-19 14:27:12 +0300

    create dart quezes program

---
+++ bin/dart_quizes_program.dart
@@ -0,0 +1,5 @@
+import 'package:dart_quizes_program/dart_quizes_program.dart' as dart_quizes_program;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_quizes_program.calculate()}!');
+}
---
+++ test/dart_quizes_program_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_quizes_program/dart_quizes_program.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

d2f3336833e9a074e10faf55f05795aaa71914c6
Mahmoud-Magdy17 <MahmoudMagdy176203@gmail.com>
2024-06-19 17:32:16 +0300

    coding all the dart quizes program

--- bin/dart_quizes_program.dart
+++ bin/dart_quizes_program.dart
@@ -1,5 +1,33 @@
-import 'package:dart_quizes_program/dart_quizes_program.dart' as dart_quizes_program;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_quizes_program.calculate()}!');
+class Question {
+  String question;
+  double answer;
+
+  Question({
+    required this.question,
+    required this.answer,
+  });
+}
+
+double askQuestion(String q) {
+  print("Q// $q ?");
+  double? userAns = double.tryParse(stdin.readLineSync()!) ?? 0.0;
+  return userAns;
+}
+
+void main() {
+  List<Question> questions = [
+    Question(question: "3 * 8 = ", answer: 24.0),
+    Question(question: "4 + 7 = ", answer: 11.0),
+    Question(question: "12 / 2 = ", answer: 6.0),
+  ];
+
+  for (var question in questions) {
+    double userAnser = askQuestion(question.question);
+    print("Your anser is : $userAnser");
+    (userAnser == question.answer)
+        ? print("Correct!")
+        : print("inCorrect! the correct anser is : ${question.answer}");
+  }
 }
